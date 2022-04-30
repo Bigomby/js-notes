bb0ef7cd5ebdf1db873ea5b345000e58d487c6b9
Diego Fernández Barrera <bigomby@gmail.com>
2022-04-30 11:56:48 +0200

    Add exercises

---
+++ clase-01/data.js
@@ -0,0 +1,20 @@
+const numeros = [12, 123, 45, 67, 2, 4, 1, 2];
+const strings = ['Juan', 'Alicia', 'Bob', 'Felipe'];
+const booleanos = [true, true, false, true, false, false, false];
+const mezcla = [true, 23, 'Alicia', 'Paco', false, 565, null];
+const objetos = [
+  {
+    nombre: 'Pantalones',
+    color: 'azul',
+    talla: 'l',
+    planta: 3,
+  },
+  {
+    nombre: 'Camiseta',
+    color: 'blanco',
+    talla: 'xl',
+    planta: 5,
+  },
+];
+
+export { numeros, strings, booleanos, mezcla, objetos };
--- clase-01/main.js
+++ clase-01/main.js
@@ -1,9 +1,25 @@
 'use strict';
+import assert from 'assert';
 
-const usuario = {
-  name: {
-    first: 'Juan',
-    last: 'García Herrera',
-  },
-  age: 31,
-};
+import { numeros, strings, booleanos, mezcla, objetos } from './data.js';
+
+// EJERCICIOS
+// Sustituye el "null" por lo que sea necesario en cada caso
+
+// Se accede al quinto elemento del array "booleanos"
+const resultado1 = null;
+assert.equal(resultado1, false);
+
+// Se accede al cuarto elemento que está en "numeros"
+const resultado2 = null;
+assert.equal(resultado2, 67);
+
+// Se accede al sexto elemento del array "mezcla"
+const resultado3 = null;
+assert.equal(resultado3, 565);
+
+// Se accede a la talla del segundo objeto en el array "objetos"
+const resultado4 = null;
+assert.equal(resultado4, 'xl');
+
+console.log('¡ENHORABUENA TODO CORRECTO!');
